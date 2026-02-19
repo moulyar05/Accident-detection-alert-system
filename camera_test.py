761b775e73551788daf21bbda2465a889cdc722d
moulyar05 <moulyar05@gmail.com>
2026-02-19 18:44:40 +0530

    First code - camera working

---
+++ camera_test.py
@@ -0,0 +1,18 @@
+import cv2
+cap = cv2.VideoCapture(0)
+print("Camera starting... Press Q to quir")
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Camera not found")
+        break
+
+    cv2.imshow("Accident Detection System", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+print("Camera closed")
---
+++ test.py
@@ -0,0 +1,4 @@
+print("Starting my AI project today")
+numbers=[10,20,30, 40 ,50]
+for num in numbers:
+    print(f"Number:{num} | Square:{num**2}")
